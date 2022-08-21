bf9876e2b17e295e94a261a1620930c3cb4e5427
JavaOPs <admin@javaops.ru>
2022-08-21 23:25:04 +0300

    12_1_init_boot_java

---
+++ README.md
@@ -0,0 +1,28 @@
+Java Enterprise Online Project
+===============================
+
+Наиболее востребованные технологии /инструменты / фреймворки Java Enterprise:
+Maven/ Spring/ Security/ JPA(Hibernate)/ REST(Jackson)/ Bootstrap(CSS)/ jQuery + plugins.
+
+- [Вступительное занятие](https://github.com/JavaOPs/topjava)
+- [Описание и план проекта](https://github.com/JavaOPs/topjava/blob/master/description.md)
+- [Wiki](https://github.com/JavaOPs/topjava/wiki)
+- [Wiki Git](https://github.com/JavaOPs/topjava/wiki/Git)
+- [Wiki IDEA](https://github.com/JavaOPs/topjava/wiki/IDEA)
+- [Демо разрабатываемого приложения](http://topjava.herokuapp.com/)
+
+### Миграция TopJava на Spring-Boot
+Исходный код с обновленными зависимостями взять из [курса BootJava](https://javaops.ru/view/bootjava):  
+[https://github.com/JavaOPs/bootjava/tree/patched](https://github.com/JavaOPs/bootjava/tree/patched)
+
+-------------------------------------------------------------
+- Stack: [JDK 17](http://jdk.java.net/17/), Spring Boot 2.7, Lombok, H2, Caffeine Cache, Swagger/OpenAPI 3.0
+- Run: `mvn spring-boot:run` in root directory.
+-----------------------------------------------------
+[REST API documentation](http://localhost:8080/swagger-ui.html)  
+Креденшелы:
+```
+Admin: admin@gmail.com / admin
+User:  user@yandex.ru / password
+Guest: guest@gmail.com / guest
+```
---
+++ src/main/java/ru/javaops/topjava/config/SecurityConfiguration.java
@@ -0,0 +1,59 @@
+package ru.javaops.topjava.config;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import ru.javaops.topjava.model.Role;
+import ru.javaops.topjava.model.User;
+import ru.javaops.topjava.repository.UserRepository;
+import ru.javaops.topjava.web.AuthUser;
+
+import java.util.Optional;
+
+@Configuration
+@EnableWebSecurity
+@Slf4j
+@AllArgsConstructor
+public class SecurityConfiguration {
+
+    public static final PasswordEncoder PASSWORD_ENCODER = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PASSWORD_ENCODER;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return email -> {
+            log.debug("Authenticating '{}'", email);
+            Optional<User> optionalUser = userRepository.findByEmailIgnoreCase(email);
+            return new AuthUser(optionalUser.orElseThrow(
+                    () -> new UsernameNotFoundException("User '" + email + "' was not found")));
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/admin/**").hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.POST, "/api/profile").anonymous()
+                .antMatchers("/api/**").authenticated()
+                .and().httpBasic()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable();
+        return http.build();
+    }
+}
