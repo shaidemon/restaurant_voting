5c9f82ff31ba8eeb6b6fae4cf9dcb3c95447df85
Dim Sh <daemon75.sh@gmail.com>
2022-09-14 18:45:35 +0900

    delete obsolete utils

--- src/main/java/ru/daemon75/voting/util/VoteUtil.java
+++ src/main/java/ru/daemon75/voting/util/VoteUtil.java
@@ -1,18 +1,15 @@
 package ru.daemon75.voting.util;
 
 import lombok.experimental.UtilityClass;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.time.LocalTime;
 
-import static ru.daemon75.voting.util.Util.isBetweenHalfOpen;
-
 @UtilityClass
 public class VoteUtil {
-//    @Value("${key.name}")
+    //    @Value("${key.name}")
     private final LocalTime CHECK_TIME = LocalTime.of(11, 0);
 
     public static Boolean isTimeForVote() {
-        return isBetweenHalfOpen(LocalTime.now(), null, CHECK_TIME);
+        return LocalTime.now().isBefore(CHECK_TIME);
     }
 }
